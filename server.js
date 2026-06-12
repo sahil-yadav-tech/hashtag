6454fc23b8e232149c268572cbc2e61828654009
Sahil Yadav <sahil9082811@gmail.com>
2026-06-12 21:22:06 +0530

    github cicd pipline

--- server.js
+++ server.js
@@ -4,5 +4,9 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.get("/")
+app.get("/health", () => {
+    return res.status(200).json({
+        message:"JAI SHREE RAM JI . JAI BAJARANG BALI JI 😍👏 My health is fine"
+    })
+})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
