57adfda9b93554097da58e8b1a2aa3272da316f8
Sahil Yadav <sahil9082811@gmail.com>
2026-06-16 12:35:49 +0530

    adding new change

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express()
 const port = 5000
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/s', (req, res) => res.send('Hello World! 2'))
+
 app.get("/health", (req, res) => {
     return res.status(200).json({
         message:"JAI SHREE RAM JI . JAI BAJARANG BALI JI 😍👏 My health is fine just becuse of you😊🥰😍"
